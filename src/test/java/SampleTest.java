2c9ee2955b76571f69379a156c4f5f168cccbd13
Zelalem Belay <zelalem.belay1@gmail.com>
2017-02-13 11:51:49 -0500

    Added travis.yml and Sampletest class

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Zelalem Belay on 2/13/2017.
+ */
+public class SampleTest
+{
+    @Test
+    public void testSample()
+    {
+        Assert.assertEquals("expected","expected");
+    }
+}
